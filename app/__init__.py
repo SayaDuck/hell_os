515c23be5fe2aa843e5ad41dd196852bff2b123e
SayaDuck <jonlee5555@gmail.com>
2021-04-27 09:38:30 -0400

    fix

--- app/__init__.py
+++ app/__init__.py
@@ -16,12 +16,12 @@ app = Flask(__name__)
 # def root():
 #     return render_template('')
 
-app.route("/login")
+@app.route("/login")
 def login():
 
     return render_template('login.html')
 
-app.route("/register")
+@app.route("/register")
 def register():
 
     return render_template('register.html')
